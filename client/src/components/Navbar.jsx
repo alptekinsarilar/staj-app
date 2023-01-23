1b66b11371add825bf171554b1e39237279a3887
Alptekin <alptekinsarilar@gmail.com>
2023-01-23 13:39:52 +0300

    Navbar implemented

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+const Navbar = () => {
+    return (
+      <nav>
+          <h1>Lvnt Ltd</h1>
+          <a>Sign Up</a>
+          <a>Login</a>
+      </nav>
+    )
+  }
+  
+  export default Navbar
