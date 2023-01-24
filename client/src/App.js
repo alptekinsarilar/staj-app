915703e2b6b75e9d1373a67acf948b7ad8cd8227
Alptekin <alptekinsarilar@gmail.com>
2023-01-24 10:36:55 +0300

    Navbar component eklendi. Tailwindcss configure edildi Html body tag'ine style verilmesi gerekli. index.html dosyasinda calismayan kismi commente aldim.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+/* import all components */
 import Navbar from "./components/Navbar";
 
+/* root routes */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+  },
+  {
+    path: "*",
+    element: <div>404 zibab vaj</div>,
+  }
+]);
+
 const App = () => {
-  return <Navbar />;
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
 };
 
 export default App;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-
 const Navbar = () => {
-    return (
-      <nav>
-          <h1>Lvnt Ltd</h1>
-          <a>Sign Up</a>
-          <a>Login</a>
-      </nav>
-    )
-  }
-  
-  export default Navbar
+  return (
+    <nav className="h-20 w-4/5 flex items-center py-5 px-5 shadow-md">
+      <h1 className="grow">Lvnt Ltd</h1>
+      <div className="m-1">
+        <a className="" href="#">
+          Sign Up
+        </a>
+        <a className="ml-1.5" href="#">Login</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
